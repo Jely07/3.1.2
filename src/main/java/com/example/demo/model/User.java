d0021bd5707d5f1ad4f834af4bc2ed29843a9970
Shegay Anzhelika <jely91@icloud.com>
2023-09-26 13:34:28 +0900

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+
+    public User(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
